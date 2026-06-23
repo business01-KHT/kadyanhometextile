8566b25f983ac7fca6d3b66909186c246b5ad34b
business01-KHT <business@kadyanhometextile.com>
2026-06-23 21:24:24 +0530

    Added script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
 
+const cards = document.querySelectorAll(".product-card");
+
+cards.forEach((card, index) => {
+card.style.opacity = "0";
+card.style.transform = "translateY(30px)";
+
+setTimeout(() => {
+card.style.transition = "all 0.8s ease";
+card.style.opacity = "1";
+card.style.transform = "translateY(0)";
+}, index * 200);
+
+});
+
+});
