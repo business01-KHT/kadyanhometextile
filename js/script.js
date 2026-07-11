f2bcd9184ca171a72f7853cf8179e3bef6499995
business01-KHT <business@kadyanhometextile.com>
2026-07-11 21:33:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,3 +25,36 @@ const observer = new IntersectionObserver(entries => {
 document.querySelectorAll('.section, .product-card, .feature').forEach(el => {
     observer.observe(el);
 });
+// Image Lightbox
+
+const images = document.querySelectorAll(".product-card img");
+
+const lightbox = document.createElement("div");
+lightbox.id = "lightbox";
+
+lightbox.innerHTML = `
+    <span id="close-lightbox">&times;</span>
+    <img id="lightbox-img">
+`;
+
+document.body.appendChild(lightbox);
+
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.getElementById("close-lightbox");
+
+images.forEach(img => {
+    img.addEventListener("click", () => {
+        lightbox.style.display = "flex";
+        lightboxImg.src = img.src;
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", function(e){
+    if(e.target === lightbox){
+        lightbox.style.display = "none";
+    }
+});
